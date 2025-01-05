ca5119caa793cc078019a3fe53adef676d371d16
Elliot <hack00mind@gmail.com>
2025-01-05 19:56:31 +0800

    feat: init words page

--- src/routes/learn/(learn).tsx
+++ src/routes/learn/(learn).tsx
@@ -1,7 +1,13 @@
+import { ModuleCard } from "~/components/module-card";
+
 export default function LearnPage() {
   return (
-    <div class="w-full">
-
+    <div class="w-full p-2 grid grid-cols-4 gap-4">
+      <ModuleCard
+        href="/learn/words"
+        title="单词记忆"
+        description="通过卡片式学习，轻松记住日语单词。"
+      />
     </div>
   )
 }
---
+++ src/routes/learn/words.tsx
@@ -0,0 +1,34 @@
+import { createResource, createSignal, Match, Show, Suspense, Switch } from "solid-js";
+import { Button } from "~/components/ui/button";
+import { WordCard } from "~/components/words/word-card";
+import { fetchNewWordsToLearn } from "~/services/words/client";
+
+export default function WordsPage() {
+  const [scope, setScope] = createSignal<string | null>()
+  const [data] = createResource(scope, fetchNewWordsToLearn)
+
+  return (
+    <div class="w-full p-2">
+      <Show when={!scope()}>
+        <div class="w-full text-center text-2xl font-semibold my-8">请选择单词书开始学习</div>
+        <div class="w-1/2 m-auto flex flex-col gap-2">
+          <Button onClick={() => setScope("N1")}>N1</Button>
+          <Button onClick={() => setScope("N2")}>N2</Button>
+          <Button onClick={() => setScope("N3")}>N3</Button>
+          <Button onClick={() => setScope("N4")}>N4</Button>
+          <Button onClick={() => setScope("N5")}>N5</Button>
+        </div>
+      </Show>
+      <Suspense fallback={<div>loading...</div>}>
+        <Switch>
+          <Match when={data.error}>
+            <div>Something wrong.</div>
+          </Match>
+          <Match when={data()}>
+            <WordCard words={data()!} />
+          </Match>
+        </Switch>
+      </Suspense>
+    </div>
+  )
+}
