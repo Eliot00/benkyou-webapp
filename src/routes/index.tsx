9d421d355d7a829bf845d079b32911d00f847707
Elliot <hack00mind@gmail.com>
2025-05-07 12:01:00 +0800

    feat: add a guide message on the homepage

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,13 @@
-import { A } from "@solidjs/router"
+import { LucideSparkles } from "lucide-solid";
 
 export default function HomePage() {
 
   return (
-    <div class="w-full p-2 space-x-2">
-      <A href="/game/kata-pair">假名配对</A>
-      <A href="/game/date-quiz">日期速答</A>
+    <div class="w-full h-full flex items-center justify-center">
+      <div class="flex items-center gap-2">
+        <LucideSparkles size="1em" />
+        欢迎回来，点击左侧菜单栏探索更多学习内容吧σ`∀´)σ
+      </div>
     </div>
   )
 }
