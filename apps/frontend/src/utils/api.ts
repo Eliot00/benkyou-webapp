b0b0ce697c145628687c2f18ef2b783bb5fcae3f
Elliot <hack00mind@gmail.com>
2025-07-28 13:42:03 +0800

    wip

--- apps/frontend/src/utils/api.ts
+++ apps/frontend/src/utils/api.ts
@@ -1,3 +1,24 @@
-import { isServer } from 'solid-js/web'
+import { getRequestEvent, isServer } from 'solid-js/web'
+import { getHeader } from 'vinxi/http'
 
 export const apiPrefix: string = isServer ? import.meta.env.VITE_API_PREFIX : '/api'
+
+export async function get(endpoint: string) {
+  let url = endpoint
+  const options: RequestInit = {
+    method: 'GET',
+    credentials: 'include',
+  }
+
+  if (isServer) {
+    url = `${import.meta.env.VITE_API_HOST}/${endpoint}`
+
+    const event = getRequestEvent()
+    const cookie = event ? (getHeader(event.nativeEvent, 'Cookie') || '' ) : ''
+
+    options.headers = {
+    }
+  }
+
+  const response = await fetch(url, options)
+}
